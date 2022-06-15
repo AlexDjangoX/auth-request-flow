db450c3f234ac57907befb3fa3bb2ee0c3899ad9
AlexDjangoX <alexmonk17@gmail.com>
2022-06-15 12:21:49 +0200

    complete

---
+++ src/controllers/token.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const createToken = (req, res) => {
+  const { username, password, profile } = req.body;
+  const payload = {};
+  payload["username"] = username;
+  payload["profile"] = profile;
+
+  const token = jwt.sign(payload, password);
+  res.json({ token });
+};
+
+const verifyToken = (req, res) => {
+  const [token] = req.headers.authorization.split(" ");
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    res.json(decodedToken);
+  } catch (e) {
+    res.status(401).json({ error: "No token" });
+  }
+};
+
+module.exports = { createToken, verifyToken };
--- src/router.js
+++ src/router.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const e = require("express");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.JWT_SECRET;
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  // extract user name, password from req.body
+  // compare with mockData
+  // if valid create and send back token or error message
 
+  const userName = mockUser.username;
+  const token = jwt.sign({ userName }, secret);
+
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  
-});
+router.get("/profile", (req, res) => {
+  const [bearer, token] = req.headers.authorization.split(" ");
 
+  try {
+    const payload = jwt.verify(token, secret);
+    res.json(mockUser.profile);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
 
 module.exports = router;
